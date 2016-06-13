8490a933ff609351968c3cb467cafe774ba8449a
c9s <yoanlin93@gmail.com>
2016-06-13 23:41:20 +0800

    upgrade facebook sdk dependency

--- Controller/OAuthFacebook.php
+++ Controller/OAuthFacebook.php
@@ -10,18 +10,17 @@ function base64_url_decode($input) {
     return base64_decode(strtr($input, '-_', '+/'));
 }
 
-function parse_signed_request($signed_request, $secret) {
-    list($encoded_sig, $payload) = explode('.', $signed_request, 2);
+function parse_signed_request($signedRequest, $secret) {
+    list($encodedSig, $payload) = explode('.', $signedRequest, 2);
 
     // decode the data
-    $sig = base64_url_decode($encoded_sig);
+    $sig = base64_url_decode($encodedSig);
     $data = json_decode(base64_url_decode($payload), true);
 
     if (strtoupper($data['algorithm']) !== 'HMAC-SHA256') {
         error_log('Unknown algorithm. Expected HMAC-SHA256');
         return null;
     }
-
     // check sig
     $expected_sig = hash_hmac('sha256', $payload, $secret, $raw = true);
     if ($sig !== $expected_sig) {
@@ -33,7 +32,6 @@ function parse_signed_request($signed_request, $secret) {
 
 class OAuthFacebook extends RequestTokenController
 {
-
     public function indexAction()
     {
         $bundle = OAuthPlugin::getInstance();
@@ -47,7 +45,6 @@ class OAuthFacebook extends RequestTokenController
         return $this->runRequestToken($provider, $callbackUrl, array('scope' => $scopes));
     }
 
-
     /**
      * here you'll get the user id who is removing or deauthorize your application
      *
@@ -56,13 +53,10 @@ class OAuthFacebook extends RequestTokenController
     public function deauthorizeCallbackAction() {
         $bundle = OAuthPlugin::getInstance();
         $config = $bundle->config('Providers.Facebook');
-
         $data       =   parse_signed_request($_REQUEST['signed_request'], $config->ClientSecret);
         $fbUserId   =   $data['user_id'];
         // $fbUserId this is the Facebook User UID who is removed your application. So you can use this id to update your database or do other tasks if required for your application
         // These methods are provided by facebook
         // http://developers.facebook.com/docs/authentication/canvas
     }
-
 }
-
